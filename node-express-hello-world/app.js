124c99cdae234a331720d50f7e493aba705ea888
Sumeet Singh <ssingh@keoxtech.com>
2026-03-14 16:45:38 +0530

    Allow external access to Node server

--- node-express-hello-world/app.js
+++ node-express-hello-world/app.js
@@ -1,16 +1,16 @@
-var express=require('express');
-var app=express();
+var express = require('express');
+var app = express();
 
-var routes=require('./routes/route.js');
+var routes = require('./routes/route.js');
 
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/',routes.home);
+app.get('/', routes.home);
 
 var port = process.env.PORT || 3000;
 
-var server=app.listen(port,function(req,res){
-    console.log("Catch the action at http://localhost:"+port);
+var server = app.listen(port, '0.0.0.0', function () {
+    console.log("Server running at http://0.0.0.0:" + port);
 });
